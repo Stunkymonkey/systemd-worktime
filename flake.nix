af3fa0f96c7b42932d62056b43e7fbcfa0e793dd
Felix Buehler <account@buehler.rocks>
2025-12-28 12:49:41 +0100

    use nix-flakes

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Listing past uptimes of systemd";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        pythonEnv = pkgs.python3.withPackages (ps: with ps; [
+          systemd-python
+        ]);
+        systemd-worktime = pkgs.writeShellApplication {
+          name = "systemd-worktime";
+          runtimeInputs = [ pythonEnv ];
+          text = ''
+            python3 ${./systemd-worktime.py} "$@"
+          '';
+        };
+      in
+      {
+        packages.default = systemd-worktime;
+
+        apps.default = {
+          type = "app";
+          program = "${systemd-worktime}/bin/systemd-worktime";
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ pythonEnv ];
+        };
+      }
+    );
+}
